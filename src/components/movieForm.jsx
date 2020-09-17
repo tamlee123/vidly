73d1bfa59f3b50392352623d6fd436ef0a3d671a
tam <cherykim123@gmail.com>
2020-09-17 15:39:53 -0700

    refactoring by populating getGenres and getMovie

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -34,19 +34,27 @@ class MovieForm extends Form {
       .max(10)
       .label("Daily Rental Rate"),
   };
-  async componentDidMount() {
+
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
   mapToViewModel(movie) {
     return {
