d9ed4ae5852cc5fde23b0d13469fdd433bc6c245
tam <cherykim123@gmail.com>
2020-09-21 22:08:51 -0700

    refactoring=moving localStorage in app and registerForm to authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,35 @@
+import jwtDecode from "jwt-decode";
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 import LoginForm from "./../components/loginForm";
 
 const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndpoint, { email, password });
-  localStorage.setItem("token", jwt);
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function loginWithJwt() {
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export function logout() {
-  localStorage.removeItem("token");
+  localStorage.removeItem(tokenKey);
+}
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
 }
 
 export default {
   login,
+  loginWithJwt,
   logout,
+  getCurrentUser,
 };
