2518e28a75080ef499fd71329063b7b9e9685c7c
tam <cherykim123@gmail.com>
2020-10-02 23:21:29 -0700

    removing if from movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -88,7 +88,7 @@ class Movies extends Component {
     const { length: count } = this.state.movies;
     const { pageSize, currentPage, searchQuery, sortColumn } = this.state;
     const { user } = this.props;
-    if (count === 0) return <p>There are no movies in the database</p>;
+
     const { totalCount, data: movies } = this.getPagedData();
     return (
       <div className="row">
